616ae2b2ec58323dabef12aae7a55c33619c68ae
1991artem <ar_puz@mail.ru>
2022-10-25 16:02:37 +0300

    fix: add log to errorHandler

--- src/helps/errorHandler.ts
+++ src/helps/errorHandler.ts
@@ -1,7 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { Response } from 'express';
+import chalk from 'chalk';
 import { ServerMessage } from '../helps/interfaces';
 
 export const serverMessage: ServerMessage = (res: Response, code: number, { errors, message }): void => {
+  if (!code) {
+    console.log(chalk.red.bold(`\n${message}\n`));
+  }
   res.status(code || 500)
     .json({ errors: errors, message: message });
 };
